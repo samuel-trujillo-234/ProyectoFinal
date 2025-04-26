cc61bbb61399610f615c1d0867bd62b94064767f
Samuel_Truji <samaletrumon@gmail.com>
2025-04-25 19:26:23 -0500

    first html

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
-from app import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
