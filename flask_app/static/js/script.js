795cb2bb6b8dbba56fd4a277186e4f280daedf4c
Roberto Alvarez <alvarez.roberto.br@gmail.com>
2025-05-04 02:12:54 -0300

    Funciones de favoritos & likes movidas para script JS en separado

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -70,33 +70,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-//
-// Funciones para favoritos & likes
-
-// Función para manejar los favoritos
-function handleFavorite(button) {
-    if (button.classList.contains('active')) {
-        button.classList.remove('active');
-        // Aquí puedes agregar la lógica para eliminar de favoritos
-    } else {
-        button.classList.add('active');
-        // Aquí puedes agregar la lógica para agregar a favoritos
-    }
-}
-
-// Función para atualizar contadores
-function updateCounter(button, action) {
-    const counter = button.querySelector('.counter');
-    let count = parseInt(counter.textContent);
-    counter.textContent = count + 1;
-    button.classList.add('active');
-    
-    // Cambiar el color del ícono si es el botón de favoritos
-    if (action === 'favorite') {
-        const icon = button.querySelector('i');
-        icon.style.color = '#ffd700';
-    }
-}
 
 //
 // Funciones para la página de configuración
